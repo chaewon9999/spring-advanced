79f9f7cffad493c43be9b6aebcbad5b0d1a9ee31
chaewon9999 <w0729@naver.com>
2025-04-18 15:23:12 +0900

    레벨 1-3: Validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,9 @@
 package org.example.expert.domain.user.dto.request;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +15,9 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
+
     @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\\\d).*$", message = "8자리 이상이어야합니다.")
+    @Size(min = 8, message = "숫자와 대문자를 포함해야 합니다.")
     private String newPassword;
 }
